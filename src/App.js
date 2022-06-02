e12cd2861199d06e8b68f5567fa660ea9cf195a8
Eliyas Hossain <eliyashossain700@gmail.com>
2022-06-03 00:38:29 +0600

    header 2 completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SmallHeader from "./Components/Header1/SmallHeader";
+import Header2 from "./Components/Header2/Header2";
 
 function App() {
   return (
     <div className="App">
       <SmallHeader></SmallHeader>
+      <Header2></Header2>
     </div>
   );
 }
