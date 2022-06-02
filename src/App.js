3090a241f19fa3a3bf620dcf4bdcd0ab1c6ad338
Eliyas Hossain <eliyashossain700@gmail.com>
2022-06-02 21:24:09 +0600

    Small Header Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import SmallHeader from "./Components/Header1/SmallHeader";
 
 function App() {
   return (
     <div className="App">
-      
+      <SmallHeader></SmallHeader>
     </div>
   );
 }
