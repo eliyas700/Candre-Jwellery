4c6a628eb95eb3355d61fc4a5c8f672bfeafb33b
Eliyas Hossain <eliyashossain700@gmail.com>
2022-06-03 01:12:03 +0600

    Header 3 Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import SmallHeader from "./Components/Header1/SmallHeader";
 import Header2 from "./Components/Header2/Header2";
+import Header3 from "./Components/Header3/Header3";
 
 function App() {
   return (
     <div className="App">
       <SmallHeader></SmallHeader>
       <Header2></Header2>
+      <Header3></Header3>
     </div>
   );
 }
---
+++ src/Components/Header3/Header3.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Header3.css";
+const Header3 = () => {
+  return (
+    <div className="border-b-2">
+      <ul className=" header3 pb-[20px] w-[85%] mx-auto flex justify-between">
+        <li>
+          <a href="/">Trending</a>
+        </li>
+        <li>
+          <a href="/">Rings</a>
+        </li>
+        <li>
+          <a href="/">Earrings</a>
+        </li>
+        <li>
+          <a href="/">Necklaces</a>
+        </li>
+        <li>
+          <a href="/">Chains</a>
+        </li>
+        <li>
+          <a href="/">Bangles</a>
+        </li>
+        <li>
+          <a href="/">Other Jewellery</a>
+        </li>
+        <li>
+          <a href="/">Collections</a>
+        </li>
+        <li>
+          <a href="/">Solitaires</a>
+        </li>
+        <li>
+          <a href="/">Gifts</a>
+        </li>
+        <li>
+          <a href="/">Wedding</a>
+        </li>
+        <li>
+          <a href="/">Sale</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header3;
