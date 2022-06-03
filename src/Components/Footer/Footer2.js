50baa5f68aaf9b4ea58c98eb8a35b4332e709a0b
Eliyas Hossain <eliyashossain700@gmail.com>
2022-06-03 17:53:08 +0600

    Footer 2 and 3 added

---
+++ src/Components/Footer/Footer2.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+const Footer2 = () => {
+  return (
+    <div>
+      <footer class="footer grid-rows-1 p-10 bg-neutral text-neutral-content">
+        <div>
+          <span class="footer-title">Services</span>
+          <a href="/" class="link link-hover">
+            Branding
+          </a>
+          <a href="/" class="link link-hover">
+            Design
+          </a>
+          <a href="/" class="link link-hover">
+            Marketing
+          </a>
+          <a href="/" class="link link-hover">
+            Advertisement
+          </a>
+        </div>
+        <div>
+          <span class="footer-title">Company</span>
+          <a href="/" class="link link-hover">
+            About us
+          </a>
+          <a href="/" class="link link-hover">
+            Contact
+          </a>
+          <a href="/" class="link link-hover">
+            Jobs
+          </a>
+          <a href="/" class="link link-hover">
+            Press kit
+          </a>
+        </div>
+        <div>
+          <span class="footer-title">Legal</span>
+          <a href="/" class="link link-hover">
+            Terms of use
+          </a>
+          <a href="/" class="link link-hover">
+            Privacy policy
+          </a>
+          <a href="/" class="link link-hover">
+            Cookie policy
+          </a>
+        </div>
+        <div>
+          <span class="footer-title">Social</span>
+          <a href="/" class="link link-hover">
+            Twitter
+          </a>
+          <a href="/" class="link link-hover">
+            Instagram
+          </a>
+          <a href="/" class="link link-hover">
+            Facebook
+          </a>
+          <a href="/" class="link link-hover">
+            Github
+          </a>
+        </div>
+        <div>
+          <span class="footer-title">Explore</span>
+          <a href="/" class="link link-hover">
+            Features
+          </a>
+          <a href="/" class="link link-hover">
+            Enterprise
+          </a>
+          <a href="/" class="link link-hover">
+            Security
+          </a>
+          <a href="/" class="link link-hover">
+            Pricing
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer2;
--- src/Components/Footer/MainFooter.js
+++ src/Components/Footer/MainFooter.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Footer1 from "./Footer1";
+import Footer2 from "./Footer2";
+import Footer3 from "./Footer3";
 
 const MainFooter = () => {
   return (
     <div className="">
       <Footer1></Footer1>
+      <Footer2></Footer2>
+      <Footer3></Footer3>
     </div>
   );
 };
