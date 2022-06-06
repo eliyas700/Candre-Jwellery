de0e17681e034b944447377b0ac14bad3efc9f93
Eliyas Hossain <eliyashossain700@gmail.com>
2022-06-06 17:37:00 +0600

    Dropdown Added

--- src/Components/Header3/Header3.js
+++ src/Components/Header3/Header3.js
@@ -1,11 +1,57 @@
 import React from "react";
 import "./Header3.css";
+import trending from "../../Assets/Trending.webp";
 const Header3 = () => {
   return (
     <div className="border-b-2">
-      <ul className=" header3 pb-[20px] w-[85%] mx-auto flex justify-between">
+      <ul className=" header3 pb-[20px] mx-16  flex justify-between">
         <li>
-          <a href="/">Trending</a>
+          <div class="dropdown dropdown-hover">
+            <label tabindex="0" class=" m-1 ">
+              <a href="/">Trending</a>
+            </label>
+            <div
+              tabindex="0"
+              class="dropdown-content  p-10 shadow bg-base-100  w-[90vw] text-left flex justify-between "
+            >
+              <ul>
+                <li className="mb-2 text-sm">
+                  <a href="/">Bestsellers</a>
+                </li>
+                <li className="mb-2 text-sm">
+                  <a href="/">Bespoke</a>
+                </li>
+                <li className="mb-2 text-sm">
+                  <a href="/">DGRP</a>
+                </li>
+                <li className="mb-2 text-sm">
+                  <a href="/">New Arrivals</a>
+                </li>
+                <li className="mb-2 text-sm">
+                  <a href="/">Our Picks</a>
+                </li>
+                <li className="mb-2 text-sm">
+                  <a href="/">Express Delivery</a>
+                </li>
+                <li className="mb-2 text-sm">
+                  <a href="/">Virtual Try On</a>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <a href="/">
+                    <img src={trending} alt="" />
+                  </a>
+                </li>
+                <li className="my-2">
+                  <a href="/" className="hover:text-[#2fbccc]">
+                    {" "}
+                    About Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </li>
         <li>
           <a href="/">Rings</a>
---
+++ src/Components/HomePage/ProductFeatures.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductFeatures = () => {
+  return <div></div>;
+};
+
+export default ProductFeatures;
